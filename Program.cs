aa0b781b414568c26103b927d1865d79bf4ff1bd
nagarjugudipati <131516565+nagarjugudipati@users.noreply.github.com>
2023-07-24 15:39:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
+    using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment14
+namespace ConAppAssignment_14
 {
     internal class Program
     {
@@ -49,6 +48,7 @@ namespace Assignment14
                 arr[j + 1] = key;
             }
         }
+
         public static void Print(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
@@ -57,27 +57,29 @@ namespace Assignment14
             }
             Console.WriteLine("\n");
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter numbers for Bubble Sort");
+            Console.WriteLine("Enter numbers");
             int[] num = new int[5];
-            for (int i = 0;i < num.Length;i++)
+            for (int i = 0; i < num.Length; i++)
             {
                 num[i] = int.Parse(Console.ReadLine());
-               
+
             }
             Console.Write("Numbers are :");
             Print(num);
 
             Console.WriteLine("Print Array without Sort");
             Print(num);
-            Stopwatch s1 = new Stopwatch();
-            s1.Start();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             BubbleSort(num);
-            s1.Stop(); 
+            stopwatch.Stop();
             Console.WriteLine("print Array after bubble sort");
             Print(num);
-            Console.WriteLine($"Arraysize is {num.Length} Time Taken for bubble sort {s1.Elapsed.TotalMilliseconds} milliseconds");
+            Console.WriteLine($" arraysize is {num.Length} Time Taken {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
+
             Console.WriteLine("==========================");
             Console.WriteLine("Enter numbers for insertion sort");
             int[] num1 = new int[5];
@@ -86,18 +88,20 @@ namespace Assignment14
                 num1[i] = int.Parse(Console.ReadLine());
 
             }
+
             Console.Write("Numbers are :");
             Print(num1);
             Console.WriteLine("Print Array without Sort");
             Print(num1);
-            Stopwatch s2 = new Stopwatch();
-            s2.Start();
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
             InsertionSort(num1);
-            s2.Stop();
+            stopwatch1.Stop();
             Console.WriteLine("print Array after Insertion sort");
             Print(num1);
-            Console.WriteLine($"Arraysize is {num1.Length} Time Taken for insertion sort {s2.Elapsed.TotalMilliseconds} milliseconds");
+            Console.WriteLine($"Arraysize is {num1.Length} Time Taken for insertion sort {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
             Console.ReadKey();
+
 
         }
     }
